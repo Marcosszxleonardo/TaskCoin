12ddeac64b1d3712f6866b226e9059f91093fd43
Gustavo <gustavinnsilva93@gmail.com>
2026-06-03 22:38:01 -0300

    Implementação inicial de design para desktop

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,6 +3,7 @@ import "../../global.css";
 import styles from "./Login.module.css";
 import api from "../services/api";
 import { Link, useNavigate } from "react-router-dom";
+import TaskCoinIcon from "../../assets/taskcoinIcon-removebg.png";
 
 export default function Login() {
   const [role, setRole] = useState("parent");
@@ -68,111 +69,112 @@ export default function Login() {
   return (
     <div className={styles.page}>
       <div className={styles.card}>
-        <img
-          className={styles.logo}
-          src="https://api.builder.io/api/v1/image/assets/TEMP/063a8574758beff68c710db5d083fb52f7c873e4?width=578"
-          alt="Taskcoin"
-        />
+        <div className={styles.logoContainer}>
+          <img
+            className={styles.logo}
+            src={TaskCoinIcon}
+            alt="Taskcoin"
+          />
 
-        <p className={styles.tagline}>
-          Disciplina que recompensa
-        </p>
-
-        <div className={styles.toggle}>
-          <button
-            type="button"
-            className={`${styles.toggleBtn} ${role === "parent"
-              ? styles.active
-              : ""
-              }`}
-            onClick={() => setRole("parent")}
-          >
-            Pai / Mãe
-          </button>
-
-          <button
-            type="button"
-            className={`${styles.toggleBtn} ${role === "child"
-              ? styles.active
-              : ""
-              }`}
-            onClick={() => setRole("child")}
-          >
-            Filho(a)
-          </button>
+          <h1 className={styles.title}>
+            TASKCOIN
+          </h1>
+          <p className={styles.tagline}>
+            Disciplina que recompensa
+          </p>
         </div>
 
-        <form
-          key={role}
-          className={styles.form}
-        >
-          <div
-            className={styles.formContent}
-          >
-            <>
-              <input
-                className={styles.input}
-                type="email"
-                placeholder="E-mail"
-                value={email}
-                onChange={(e) =>
-                  setEmail(e.target.value)
-                }
-                required
-              />
+        <div className={styles.contentContainer}>
 
-              <input
-                className={styles.input}
-                type="password"
-                placeholder="Senha"
-                value={senha}
-                onChange={(e) =>
-                  setSenha(e.target.value)
-                }
-                required
-              />
-            </>
+          <div className={styles.toggle}>
+            <button
+              type="button"
+              className={`${styles.toggleBtn} ${role === "parent"
+                ? styles.active
+                : ""
+                }`}
+              onClick={() => setRole("parent")}
+            >
+              Pai / Mãe
+            </button>
+
+            <button
+              type="button"
+              className={`${styles.toggleBtn} ${role === "child"
+                ? styles.active
+                : ""
+                }`}
+              onClick={() => setRole("child")}
+            >
+              Filho(a)
+            </button>
           </div>
 
-          {role === "parent" ? (
-            <button
-              type="submit"
-              className={styles.btnEntrar}
-              onClick={handleLoginParent}
-            >
-              Entrar como Pai/Mãe
-            </button>
-          ) : (
-            <button
-              type="submit"
-              className={styles.btnEntrar}
-              onClick={handleLoginChild}
-            >
-              Entrar como Filho(a)
-            </button>
-          )}
-
-        </form>
-
-        <div className={styles.accountRow}>
-          <button
-            type="button"
-            className={styles.accountLink}
+          <form
+            key={role}
+            className={styles.form}
           >
-            Não possui uma conta?
+            <div
+              className={styles.formContent}
+            >
+              <>
+                <input
+                  className={styles.input}
+                  type="email"
+                  placeholder="E-mail"
+                  value={email}
+                  onChange={(e) =>
+                    setEmail(e.target.value)
+                  }
+                  required
+                />
+
+                <input
+                  className={styles.input}
+                  type="password"
+                  placeholder="Senha"
+                  value={senha}
+                  onChange={(e) =>
+                    setSenha(e.target.value)
+                  }
+                  required
+                />
+              </>
+            </div>
+
+            {role === "parent" ? (
+              <button
+                type="submit"
+                className={styles.btnEntrar}
+                onClick={handleLoginParent}
+              >
+                Entrar como Pai/Mãe
+              </button>
+            ) : (
+              <button
+                type="submit"
+                className={styles.btnEntrar}
+                onClick={handleLoginChild}
+              >
+                Entrar como Filho(a)
+              </button>
+            )}
+
+          </form>
+
+          <div className={styles.accountRow}>
+            <button
+              type="button"
+              className={styles.accountLink}
+            >
+              Não possui uma conta?
+            </button>
+          </div>
+
+          <button className={styles.btnCadastrar} onClick={HandleCadastrar}>
+            Cadastrar
           </button>
         </div>
-
-        <button className={styles.btnCadastrar} onClick={HandleCadastrar}>
-          Cadastrar
-        </button>
-
-        {/* <Link
-          to="/cadastrar"
-          className={styles.btnCadastrar}
-        >
-          Cadastrar
-        </Link> */}
       </div>
     </div>
   );
