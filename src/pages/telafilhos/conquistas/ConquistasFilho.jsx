b49d8c307d8a3087e07df9e1dfd25bf98334967d
Gustavo <gustavinnsilva93@gmail.com>
2026-06-10 13:12:47 -0300

    Pequenos ajustes em bugs visuais

--- src/pages/telafilhos/conquistas/ConquistasFilho.jsx
+++ src/pages/telafilhos/conquistas/ConquistasFilho.jsx
@@ -96,7 +96,7 @@ export default function ConquistasFilho() {
 
             <div className={styles.progressTop}>
               {usuario.nivel && (
-                <h2>Nv. {usuario.nivel.nivel} - <span className={styles.levelTitle}>{usuario.nivel.titulo_nivel}</span></h2>
+                <h2>Nv. {usuario.nivel.nivel} - <span className={styles.levelTitle}>{usuario.nivel.titulo_nivel}ㅤ</span></h2>
               )}
               <span>
                 <Counter target={porcentagemProgresso} duration={1000}></Counter>%
@@ -114,7 +114,7 @@ export default function ConquistasFilho() {
             )}
           </section>
         </div>
-        
+
         <div className={styles.rightSide}>
           <section className={styles.section}>
 
